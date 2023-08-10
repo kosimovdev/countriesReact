d57f2cc710910962d517ab9f7466779e33babe1f
kosimovdev <kosimov.oyatillo@gmail.com>
2023-08-10 11:33:09 +0500

    data countries render from api

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import Navbar from './components/Navbar';
+// import Header from './components/Header'
+// import Layout from './components/Layout'
+// import Footer from './components/Footer'
+import Posts from './components/Posts';
+// import Dbjson from './components/Dbjson';
+function App() {
+
+  return (
+    <>
+      {/* <Navbar />
+      <Header />
+      <Layout/>
+      <Footer /> */}
+      {/* <Dbjson/> */}
+      <Posts/>
+    </>
+  )
+}
+
+export default App
+ 
